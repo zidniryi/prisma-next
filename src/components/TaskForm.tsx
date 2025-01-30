3787e8f754b4aa6248a717e8c8307268022358ff
zidniryi <zidniryi123@gmail.com>
2025-01-30 07:18:10 +0700

    feat: [risma create

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,8 +1,27 @@
+import prisma from "@/utils/db";
 import React from 'react'
 
-const TaskForm = () => {
+const createTask = async (form: FormData) => {
+  'use server';
+  const content = form.get('content');
+  await prisma.task.create({
+    data: {
+      // @ts-ignore
+      content,
+    }
+  })
+
+}
+
+const TaskForm = async () => {
   return (
-    <div>TaskForm</div>
+    <form action={createTask}>
+      <div className="join w-full">
+        <input type="text" className="input input-primary join-item w-full" placeholder="content" name="content" required />
+        <button type="submit" className="btn btn-primary">Create Task</button>
+
+      </div>
+    </form>
   )
 }
 
